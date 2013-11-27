7791cc11d6b5b7aef67265947383d1f3ef1995d4
Nobby <nobby451@gmail.com>
2013-11-28 01:01:07 +0900

    もはやidは必要ないのでつけないようにする
    ulを生成してidをセットする処理をなくす
    ulを生成してreduceするところまでをfunctionにまとめる

--- Bookmarks/listMaker.js
+++ Bookmarks/listMaker.js
@@ -1,30 +1,28 @@
 "use strict";
 addEventListener("load", function(){
     chrome.bookmarks.getTree(function(root) {
-        var parse = function (previousValue, currentValue, index, array) {
-            var li = document.createElement("li");
-            if (currentValue.children) {
-                li.textContent = currentValue.title;
-                li.classList.add("dir");
-                li.addEventListener("click", function (ev) {
-                    ev.stopPropagation();
-                    this.classList.toggle("hidden");
-                });
-                var ul = document.createElement("ul");
-                ul.id = currentValue.id;
-                li.appendChild(currentValue.children.reduce(parse, ul));
-            } else {
-                var a = document.createElement("a");
-                a.textContent = currentValue.title;
-                a.href = currentValue.url;
-                li.classList.add("page");
-                li.appendChild(a);
-            }
-            previousValue.appendChild(li);
-            return previousValue;
+        var parse = function (array) {
+            return array.reduce(function (previousValue, currentValue, index, array) {
+                var li = document.createElement("li");
+                if (currentValue.children) {
+                    li.textContent = currentValue.title;
+                    li.classList.add("dir");
+                    li.addEventListener("click", function (ev) {
+                        ev.stopPropagation();
+                        this.classList.toggle("hidden");
+                    });
+                    li.appendChild(parse(currentValue.children));
+                } else {
+                    var a = document.createElement("a");
+                    a.textContent = currentValue.title;
+                    a.href = currentValue.url;
+                    li.classList.add("page");
+                    li.appendChild(a);
+                }
+                previousValue.appendChild(li);
+                return previousValue;
+            }, document.createElement("ul"));
         };
-        var ul = document.createElement("ul");
-        ul.id = "undefined";
-        document.body.appendChild(root.reduce(parse, ul));
+        document.body.appendChild(parse(root));
     });
 });
