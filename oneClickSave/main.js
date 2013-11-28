8d835ceb4f0c1607be2de026d174200f40a8dda1
Ryota Aoki <marrontan619.emobile.ne.jp>
2013-11-29 02:21:21 +0900

    チェックボックスの状態を保持する方法を調べてみた

--- oneClickSave/main.js
+++ oneClickSave/main.js
@@ -1,11 +1,13 @@
 "use strict";
 addEventListener("load", function() {
     var checkBox = document.getElementById("checkBox");
+    checkBox.checked = Boolean(localStorage.getItem("checked"));
     checkBox.addEventListener("change", function() {
-       if(checkBox.checked == true) {
+        var checked = (this.checked) ? "1" : "";
+        localStorage.setItem("checked", checked);
+        if(checkBox.checked == true) {
            chrome.tabs.executeScript(null, {file: "attachDownload.js"});
        }
-        
     });
     
 });
