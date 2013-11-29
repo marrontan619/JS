b1c254cf6b2063d893c6e13f6d5b0f3ac2cd2f97
Nobby <nobby451@gmail.com>
2013-11-29 12:23:09 +0900

    冗長な記述を削除
    三項演算子の条件はプロパティーを取ってきてるだけなので括弧は不要
    ifの条件式はtrueと==で比較しているので二重に比較していることになってしまう
    ===の比較であれば型も一致していないとtrueにならないので意味がある

--- oneClickSave/main.js
+++ oneClickSave/main.js
@@ -3,9 +3,9 @@ addEventListener("load", function() {
     var checkBox = document.getElementById("checkBox");
     checkBox.checked = Boolean(localStorage.getItem("checked"));
     checkBox.addEventListener("change", function() {
-        var checked = (this.checked) ? "1" : "";
+        var checked = this.checked ? "1" : "";
         localStorage.setItem("checked", checked);
-        if(checkBox.checked == true) {
+        if (checkBox.checked) {
            chrome.tabs.executeScript(null, {file: "attachDownload.js"});
        }
     });
