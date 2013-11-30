c8fee56189c0042ba69c1856c803cf5004ce78c8
Ryota <marrontan619@gmail.com>
2013-11-30 23:39:40 +0900

    作業中：拡張機能のメッセージはJSONで送られるらしく、sendMessage()にhtml要素を入れると怒られる

---
+++ AddressSetter/contextMenu.js
@@ -0,0 +1,39 @@
+// アイテムがクリックされた時の動作を登録
+function onClickHandler(info, tab) {
+  if (info.menuItemId == "e_mail_1") {
+      chrome.tabs.executeScript(null, {code: 'chrome.storage.local.get(function(items) {document.activeElement.value += items["e_mail_1"]})'});
+  } else if (info.menuItemId == "e_mail_2") {
+      chrome.tabs.executeScript(null, {code: 'chrome.storage.local.get(function(items) {document.activeElement.value += items["e_mail_2"]})'});
+  } else if (info.menuItemId == "name") {
+      chrome.tabs.executeScript(null, {code: 'chrome.storage.local.get(function(items) {document.activeElement.value += items["name"]})'});
+  } else if (info.menuItemId == "address") {
+      chrome.tabs.executeScript(null, {code: 'chrome.storage.local.get(function(items) {document.activeElement.value += items["address"]})'});
+  } else if (info.menuItemId == "template") {
+      chrome.tabs.executeScript(null, {code: 'chrome.storage.local.get(function(items) {document.activeElement.value += items["template"]})'});
+  }
+};
+
+chrome.contextMenus.onClicked.addListener(onClickHandler);
+
+// インストール時に作成
+chrome.runtime.onStartup.addListener(function() {
+    var CreateProperties = function (id, title, parentId) {
+        this.id = id;
+        this.title = title;
+        this.contexts = ["editable"];
+        this.parentId = parentId;
+    };
+    chrome.contextMenus.create(new CreateProperties("name", "名前"));
+// 子要素を入れると、親要素がクリック出来ないので、今のところ凍結
+//  chrome.contextMenus.create(
+//      {"title": "苗字のみ", "parentId": "name", "id": "familyName", "contexts": ["editable"]});
+//  chrome.contextMenus.create(
+//      {"title": "名前のみ", "parentId": "name", "id": "firstName", "contexts": ["editable"]});
+
+    chrome.contextMenus.create(new CreateProperties("e_mail_1", "メールアドレス1"));
+    chrome.contextMenus.create(new CreateProperties("e_mail_2", "メールアドレス2"));
+    chrome.contextMenus.create(new CreateProperties("address", "住所"));
+
+    chrome.contextMenus.create(new CreateProperties("template", "テンプレート"));
+
+});
---
+++ AddressSetter/options.js
@@ -0,0 +1,22 @@
+$(function() {
+    var storage = chrome.storage.local;
+    var setOption = function() {
+        var elem = $(this).prev();
+        var item = {};
+        var id = elem.attr("id");
+        var value = elem.val();
+        item[id] = value;
+        storage.set(item);
+    };
+    
+    $("button.setButton").click(setOption);
+    
+    storage.get(function(items) {
+        var contexts = ["name", "e_mail_1", "e_mail_2", "address", "template"];
+        var counter = 0;
+        var setVal = function() {
+            $(this).val(items[contexts[counter++]]);
+        };
+        $("button.setButton").prev().each(setVal);
+    });
+});
