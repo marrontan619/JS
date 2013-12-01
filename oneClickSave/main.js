277ae8ce15f9c0311f4f8bc1e04d26fb76609c41
Ryota Aoki <marrontan619.emobile.ne.jp>
2013-12-02 00:31:57 +0900

    整理のためのコミット。

--- oneClickSave/main.js
+++ oneClickSave/main.js
@@ -6,8 +6,10 @@ addEventListener("load", function() {
         var checked = this.checked ? "1" : "";
         localStorage.setItem("checked", checked);
         if (checkBox.checked) {
-            chrome.runtime.onMessage.addListener(function(title) {
-                chrome.downloads.download(localStorage.getItem(title));
+            chrome.runtime.onMessage.addListener(function(src) {
+//                console.dir(src);
+                //そもそもJSONやStringではなく、ファイル形式でないとダウンロードできないのでは
+                chrome.downloads.download(JSON.parse(src));
             });
             chrome.tabs.executeScript(null, {file: "attachDownload.js"});
        }
