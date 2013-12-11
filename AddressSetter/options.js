f231b77960acff362b4aa98873f56d518b9e7fed
Ryota <marrontan619@gmail.com>
2013-12-12 00:01:10 +0900

    オプションページで、現在の値を表示させるようにしたい（できてない）

--- AddressSetter/options.js
+++ AddressSetter/options.js
@@ -1,10 +1,14 @@
 addEventListener("load", function() {
-    var contexts = new Array("name", "e_mail_1", "e_mail_2", "address", "template");
+    var contexts = ["name", "e_mail_1", "e_mail_2", "address", "template"];
     var storage = chrome.storage.local;
     
     for (var i = 0; i < contexts.length; i++) {
         var context = contexts[i];
-        document.getElementById(context).addEventListener("change", function() {
+        var elem = document.getElementById(context);
+        storage.get(function(items) {
+            elem.value = items[context];
+        });
+        elem.addEventListener("change", function() {
             var item = {};
             var id = this.id;
             var value = this.value;
