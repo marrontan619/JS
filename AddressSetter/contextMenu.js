e39d858546322348ea01f2d1a5b288eabad9528f
r-aoki <ryota.aoki@it-forest.com>
2013-12-11 23:59:54 +0900

    入力を追記する形式に変更

--- AddressSetter/contextMenu.js
+++ AddressSetter/contextMenu.js
@@ -1,15 +1,15 @@
 // アイテムがクリックされた時の動作を登録
 function onClickHandler(info, tab) {
   if (info.menuItemId == "e_mail_1") {
-      chrome.tabs.executeScript(null, {code: 'chrome.storage.local.get(function(items) {document.activeElement.value = items["e_mail_1"]})'});
+      chrome.tabs.executeScript(null, {code: 'chrome.storage.local.get(function(items) {document.activeElement.value += items["e_mail_1"]})'});
   } else if (info.menuItemId == "e_mail_2") {
-      chrome.tabs.executeScript(null, {code: 'chrome.storage.local.get(function(items) {document.activeElement.value = items["e_mail_2"]})'});
+      chrome.tabs.executeScript(null, {code: 'chrome.storage.local.get(function(items) {document.activeElement.value += items["e_mail_2"]})'});
   } else if (info.menuItemId == "name") {
-      chrome.tabs.executeScript(null, {code: 'chrome.storage.local.get(function(items) {document.activeElement.value = items["name"]})'});
+      chrome.tabs.executeScript(null, {code: 'chrome.storage.local.get(function(items) {document.activeElement.value += items["name"]})'});
   } else if (info.menuItemId == "address") {
-      chrome.tabs.executeScript(null, {code: 'chrome.storage.local.get(function(items) {document.activeElement.value = items["address"]})'});
+      chrome.tabs.executeScript(null, {code: 'chrome.storage.local.get(function(items) {document.activeElement.value += items["address"]})'});
   } else if (info.menuItemId == "template") {
-      chrome.tabs.executeScript(null, {code: 'chrome.storage.local.get(function(items) {document.activeElement.value = items["template"]})'});
+      chrome.tabs.executeScript(null, {code: 'chrome.storage.local.get(function(items) {document.activeElement.value += items["template"]})'});
   }
 };
 
