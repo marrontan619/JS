fed3d4ab1538b90116e9e828036ada20e66fb57c
Ryota <marrontan619@gmail.com>
2013-12-12 02:28:01 +0900

    現在の設定をオプションページの初期値として入れることに成功しました。

--- AddressSetter/options.js
+++ AddressSetter/options.js
@@ -1,19 +1,22 @@
 addEventListener("load", function() {
     var contexts = ["name", "e_mail_1", "e_mail_2", "address", "template"];
     var storage = chrome.storage.local;
+    var setOption = function() {
+        var item = {};
+        var id = this.id;
+        var value = this.value;
+        item[id] = value;
+        storage.set(item);
+    };
     
     for (var i = 0; i < contexts.length; i++) {
         var context = contexts[i];
-        var elem = document.getElementById(context);
-        storage.get(function(items) {
-            elem.value = items[context];
-        });
-        elem.addEventListener("change", function() {
-            var item = {};
-            var id = this.id;
-            var value = this.value;
-            item[id] = value;
-            storage.set(item);
-        });
+        document.getElementById(context).addEventListener("change", setOption);
     }
+    storage.get(function(items) {
+        for(var i = 0; i < contexts.length; i++) {
+            var context = contexts[i];
+            document.getElementById(context).value = items[context];
+        }
+    });
 });
