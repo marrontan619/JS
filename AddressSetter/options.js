50b2d4bcbdb59604611cf23ff4271bd9262f2dde
Ryota Aoki <marrontan619.emobile.ne.jp>
2013-12-13 01:59:35 +0900

    onchangeは分かりづらいので、設定の変更を明示的な操作に変更。 jquery使ってみた。

--- AddressSetter/options.js
+++ AddressSetter/options.js
@@ -1,22 +1,22 @@
-addEventListener("load", function() {
+$(function() {
     var contexts = ["name", "e_mail_1", "e_mail_2", "address", "template"];
     var storage = chrome.storage.local;
     var setOption = function() {
+        var elem = $(this).prev();
         var item = {};
-        var id = this.id;
-        var value = this.value;
+        var id = elem.attr("id");
+        var value = elem.val();
         item[id] = value;
         storage.set(item);
     };
     
-    for (var i = 0; i < contexts.length; i++) {
-        var context = contexts[i];
-        document.getElementById(context).addEventListener("change", setOption);
-    }
+    $("button.setButton").click(setOption);
+    
     storage.get(function(items) {
-        for(var i = 0; i < contexts.length; i++) {
-            var context = contexts[i];
-            document.getElementById(context).value = items[context];
-        }
+        var counter = 0;
+        var setVal = function() {
+            $(this).val(items[contexts[counter++]]);
+        };
+        $("button.setButton").prev().each(setVal);
     });
 });
