4f71d45eae3d281e4e297cb4efcefd738716c450
Ryota <marrontan619@gmail.com>
2013-12-13 01:57:12 +0900

    動かなくなったので、イベントをonInstalledに戻した

--- AddressSetter/contextMenu.js
+++ AddressSetter/contextMenu.js
@@ -16,7 +16,7 @@ function onClickHandler(info, tab) {
 chrome.contextMenus.onClicked.addListener(onClickHandler);
 
 // インストール時に作成
-chrome.runtime.onStartup.addListener(function() {
+chrome.runtime.onInstalled.addListener(function() {
     var CreateProperties = function (id, title, parentId) {
         this.id = id;
         this.title = title;
