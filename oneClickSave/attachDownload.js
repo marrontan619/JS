5bede863c71faa975c34d1718d515288555bdaf8
Ryota <marrontan619@gmail.com>
2013-12-13 19:37:39 +0900

    作業中： ・タブ内とチェックボックスの状態を共有できるようになった。 ・ポップアップが開いてなくてもダウンロードができるようになった。 残課題：removeEventListener()ができない。

--- oneClickSave/attachDownload.js
+++ oneClickSave/attachDownload.js
@@ -10,15 +10,17 @@ var attachDownload = function(ev) {
         var dlTarget = {
             url : srcImg.src,
             filename : (srcImg.title) ? srcImg.title : null
-        }
+        };
         chrome.runtime.sendMessage(dlTarget);
     }
-}
+};
 
-//popup.htmlのチェックボックスの状態を判定できる方法検討中
-//console.dir(localStorage.getItem("checked"));
-//if (Boolean(localStorage.getItem("checked"))) {
-    document.addEventListener("click", attachDownload);
-//} else {
-//    document.removeEventListener("click", attachDownload);
-//}
+chrome.storage.local.get(function(items) {
+    if(Boolean(items["checked"])) {
+        document.addEventListener("click", attachDownload);
+        console.dir("attached");
+    } else {
+        document.removeEventListener("click", attachDownload);
+        console.dir("detached");
+    }
+});
--- oneClickSave/main.js
+++ oneClickSave/main.js
@@ -1,16 +1,15 @@
 "use strict";
 addEventListener("load", function() {
     //チェックボックスの状態についての処理
+    var storage = chrome.storage.local;
     var checkBox = document.getElementById("checkBox");
-    checkBox.checked = Boolean(localStorage.getItem("checked"));
+    storage.get(function(items) {
+        checkBox.checked = Boolean(items["checked"]);
+    });
     //チェックボックスがクリックされたら、ローカルストレージを更新して、スクリプトを呼び出す
     checkBox.addEventListener("change", function() {
         var checked = this.checked ? "1" : "";
-        localStorage.setItem("checked", checked);
-        chrome.runtime.onMessage.addListener(function(request) {
-            //ポップアップ（の開発者ツールとしてウインドウ）が開いていないと動かないので、要検討
-            chrome.downloads.download(request);
-        });
+        storage.set({checked: checked});
         chrome.tabs.executeScript(null, {file: "attachDownload.js"});
     });
     
