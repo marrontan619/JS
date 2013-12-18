e2b1c702b203e4c9beedbfdee69c623b6244322c
Ryota <marrontan619@gmail.com>
2013-12-19 01:26:00 +0900

    ストレージと画面表示の状態を判定（しているように見える）ボタン制御を追加。 これで一旦完成。

--- AddressSetter/options.js
+++ AddressSetter/options.js
@@ -1,6 +1,7 @@
 $(function() {
     var storage = chrome.storage.local;
     var setOption = function() {
+        $(this).attr("disabled", true);
         var elem = $(this).prev();
         var item = {};
         var id = elem.attr("id");
@@ -9,7 +10,13 @@ $(function() {
         storage.set(item);
     };
     
-    $("button.setButton").click(setOption);
+    var buttons = $("button.setButton")
+    
+    buttons.attr("disabled", true);
+    buttons.click(setOption);
+    buttons.prev().keydown(function() {
+        $(this).next().attr("disabled", false);
+    })
     
     storage.get(function(items) {
         var contexts = ["name", "e_mail_1", "e_mail_2", "address", "template"];
