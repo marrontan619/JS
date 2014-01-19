2e3a1fb2b2ecb18c5039a23230e7456bbcdde4d0
Ryota <marrontan619@gmail.com>
2014-01-20 01:34:11 +0900

    作業中：項目、内容がすべてユーザーにより設定できるようにリファクタリング開始
    現状：要素の追加、削除を画面から操作できるように設定
    残課題：動的にIDを割り振る、オプション画面を完全に動的に作成する

--- AddressSetter/options.js
+++ AddressSetter/options.js
@@ -10,13 +10,39 @@ $(function() {
         storage.set(item);
     };
     
-    var buttons = $("button.setButton");
-    
-    buttons.attr("disabled", true);
-    buttons.click(setOption);
-    buttons.prev().keydown(function() {
+    var enableSetButton = function() {
         $(this).next().attr("disabled", false);
-    })
+    }
+    
+    var setButtons = $("button.setButton");
+    
+    setButtons.attr("disabled", true);
+    setButtons.click(setOption);
+    setButtons.prev().bind("input", enableSetButton);
+    
+    
+    $("button.deleteButton").click(function() {
+        $(this).parent().parent().remove();
+    });
+    
+    var dl = $("dl");
+    $("button.addButton").click(function() {
+        var itemSetDiv = $("<div/>").addClass("itemSet");
+        
+        var dt = $("<dt/>");
+        $("<input/>").attr("type", "text").appendTo(dt);
+        dt.append("：");
+        
+        var dd = $("<dd/>");
+        $("<input/>").attr("type", "text").attr("id", "id").bind("input", enableSetButton).appendTo(dd);
+        $("<button/>").addClass("setButton").text("設定").attr("disabled", true).appendTo(dd);
+        $("<button/>").addClass("deleteButton").text("削除").appendTo(dd);
+        
+        itemSetDiv.append(dt);
+        itemSetDiv.append(dd);
+        dl.append(itemSetDiv);
+    });
+    
     
     storage.get(function(items) {
         var contexts = ["name", "e_mail_1", "e_mail_2", "address", "template"];
